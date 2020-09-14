836ef38d2a397517a18f55fb7fcb960d3e49bc96
jedi5218 <jedi5218@gmail.com>
2020-09-14 13:04:51 +0300

    Add gunicorn and heroku Procfile

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
