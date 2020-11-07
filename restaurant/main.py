e1111d70284746a89ac92dcf70c8cee9a82dc8bb
Illia Shestakov <illia@ishestakov.com>
2020-11-08 01:07:41 +0200

    Semyon Semyonich back in action (I'm tired)

--- restaurant/main.py
+++ restaurant/main.py
@@ -1,14 +1,12 @@
 import random
+import os
 
 from flask import Flask, render_template
 app = Flask(__name__)
+app.jinja_env.globals['CAFE_NAME'] = os.environ['CAFE_NAME']
 
 INGREDIENTS = [ 'guano', 'feces', 'poop', 'shit', 'fudge' ]
 
 @app.route('/')
 def hello_world():
-    return render_template('index.jinja2', ingredient=random.choice(INGREDIENTS))
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    return render_template('index.html', ingredient=random.choice(INGREDIENTS))
