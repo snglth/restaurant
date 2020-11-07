b912ea7f573d0883ddf6fdc02cde10605370f195
Illia Shestakov <illia@ishestakov.com>
2020-11-08 00:26:33 +0200

    Templates are power

--- restaurant/main.py
+++ restaurant/main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+import random
+
+from flask import Flask, render_template
 app = Flask(__name__)
+
+INGREDIENTS = [ 'guano', 'feces', 'poop', 'shit', 'fudge' ]
 
 @app.route('/')
 def hello_world():
-    return 'Golubtsy with guano served! Yay!'
+    return render_template('index.jinja2', ingredient=random.choice(INGREDIENTS))
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
