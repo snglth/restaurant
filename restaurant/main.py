e539d49b09a28d69df777c0321e8b710abb3d004
Illia Shestakov <illia@ishestakov.com>
2020-11-08 00:09:05 +0200

    Be happy

--- restaurant/main.py
+++ restaurant/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Golubtsy with guano served'
+    return 'Golubtsy with guano served! Yay!'
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
