8ec13438071a052ff924b8e0a775a39f08150576
jedi5218 <jedi5218@gmail.com>
2020-11-08 21:04:50 +0200

    Remove database fill, Remove unnecessary imports and comments

--- restaurant/app.py
+++ restaurant/app.py
@@ -1,18 +1,8 @@
-import random
 import os
-
-from flask import Flask, render_template
+from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from restaurant.schema import Base as schema_base
-import restaurant.schema as schema
-
-
-def init_ingredients(sessionmaker, ingredients_list):
-    session = sessionmaker()
-    for ingredient in ingredients_list:
-        session.add(schema.Ingredient(title=ingredient, unit=schema.Units.piece))
-    session.commit()
 
 
 def make_app():
@@ -23,20 +13,11 @@ def make_app():
     DB_URL = (
         os.environ.get("DB_URL") or "postgresql://postgres:123@172.17.0.2/restaurant"
     )
-    # engine = create_engine('sqlite:///:memory:', echo=True)
     engine = create_engine(DB_URL, echo=True)
     schema_base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-    INGREDIENTS = ["guano", "feces", "poop", "shit", "fudge"]
-    init_ingredients(Session, INGREDIENTS)
     return app
 
-
-# @app.route('/')
-# def hello_world():
-#     session = Session()
-#     ingredients = session.query(schema.Ingredient).all()
-#     return render_template('index.html', ingredient=random.choice(ingredients).title)
 
 app = make_app()
