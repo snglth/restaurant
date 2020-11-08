41c7b554e41f025b73e0730c07081564c1e89bbd
jedi5218 <jedi5218@gmail.com>
2020-11-08 21:06:44 +0200

    Db schema (#3)
    * Add schema and basic DB setup
    
    * Add psycopg2 dependency
    
    * Add example ingredient query
    
    * Replace database url with an env variable, fix formatting
    
    * Remove database fill, Remove unnecessary imports and comments

--- restaurant/app.py
+++ restaurant/app.py
@@ -1,11 +1,22 @@
 import os
-
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from restaurant.schema import Base as schema_base
 
 
 def make_app():
     app = Flask(__name__)
-    app.jinja_env.globals["CAFE_NAME"] = os.environ["CAFE_NAME"]
+    app.jinja_env.globals["CAFE_NAME"] = (
+        os.environ.get("CAFE_NAME") or "Semyon's Special"
+    )
+    DB_URL = (
+        os.environ.get("DB_URL") or "postgresql://postgres:123@172.17.0.2/restaurant"
+    )
+    engine = create_engine(DB_URL, echo=True)
+    schema_base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
     return app
 
 
---
+++ restaurant/schema.py
@@ -0,0 +1,59 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, Sequence, Date, ForeignKey, Enum
+import enum
+
+Base = declarative_base()
+
+
+class Recipe(Base):
+    __tablename__ = "Recipe"
+    id = Column(Integer, Sequence("recipe_id_seq"), primary_key=True)
+    title = Column(String)
+    brief_title = Column(String)
+    creation_date = Column(Date)
+    author = Column(Integer)
+    category = Column(String)
+
+
+class Author(Base):
+    __tablename__ = "Author"
+    id = Column(Integer, Sequence("author_id_seq"), primary_key=True)
+    full_name = Column(String)
+    date_of_birth = Column(Date)
+
+
+class Units(enum.Enum):
+    gram = "g."
+    piece = "pcs."
+    mililitre = "ml."
+
+
+class Ingredient(Base):
+    __tablename__ = "Ingredient"
+    title = Column(String, primary_key=True)
+    unit = Column(Enum(Units))
+
+
+class RecipeIngredient(Base):
+    __tablename__ = "RecipeIngredient"
+    recipe = Column(Integer, ForeignKey("Recipe.id"), primary_key=True)
+    ingredient = Column(String, ForeignKey("Ingredient.title"), primary_key=True)
+    quantity = Column(Float)
+
+
+class Menu(Base):
+    __tablename__ = "Menu"
+    id = Column(Integer, Sequence("menu_id_seq"), primary_key=True)
+    company = Column(String)
+    date = Column(Date)
+
+
+class MenuItem(Base):
+    __tablename__ = "MenuItem"
+    menu = Column(Integer, ForeignKey("Menu.id"), primary_key=True)
+    recipe = Column(Integer, ForeignKey("Recipe.id"), primary_key=True)
+
+
+class Category(Base):
+    __tablename__ = "Category"
+    title = Column(String, primary_key=True)
